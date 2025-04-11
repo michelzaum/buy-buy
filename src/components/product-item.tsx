493d3448f5cde63391159fb4858b724588d6a58f
michelzaum <michel.oliveira999@hotmail.com>
2025-04-11 01:41:39 -0300

    feat: adjusting product item size and displaying description

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -2,21 +2,24 @@ import Image from "next/image";
 import { Card, CardContent, CardFooter } from "./ui/card";
 import { Product } from "@prisma/client";
 
-export function ProductItem({ imageUrl, name, price }: Product) {
+export function ProductItem({ imageUrl, name, description, price }: Product) {
   return (
-    <Card className="max-w-sm hover:cursor-pointer hover:shadow-lg transition-shadow duration-300 ease-in-out">
+    <Card className="w-full md:max-w-2xs hover:cursor-pointer hover:shadow-lg transition-shadow duration-300 ease-in-out">
       <CardContent className="flex justify-center">
         <Image
           alt="Product Image"
           src={imageUrl}
           width={300}
           height={250}
-          className="rounded-md max-h-[400px]"
+          className="rounded-md h-[200px] bg-contain object-cover"
         />
       </CardContent>
       <CardFooter>
-        <div className="flex flex-col gap-1">
-          <span>{name}</span>
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-1">
+            <span className="text-xl">{name}</span>
+            <span>{description}</span>
+          </div>
           <strong>{price}</strong>
         </div>
       </CardFooter>
