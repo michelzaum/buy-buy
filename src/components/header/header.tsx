e040382ab32b051827b347e175b2aab04394f442
michelzaum <michel.oliveira999@hotmail.com>
2025-04-14 20:45:48 -0300

    feat: create header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="flex justify-center py-6">
+      <div className="w-full max-w-3xl flex justify-center items-center">
+        <h1>Header</h1>
+      </div>
+    </header>
+  );
+}
