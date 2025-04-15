092e4a0ced7f747cc274238b890821f52339aec6
michelzaum <michel.oliveira999@hotmail.com>
2025-04-14 22:21:46 -0300

    feat: create new sidebar and displaying it only on product list page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,6 @@
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { ChevronLeft } from "lucide-react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
-import { Separator } from "@radix-ui/react-separator";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import { ProductList } from "@/components/product/product-list";
 import { db } from "@/lib/db";
 
@@ -28,10 +16,20 @@ export default async function Home() {
   });
 
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <div className="flex flex-col gap-4 md:flex-row md:flex-wrap md:gap-8">
-        <ProductList products={products} />
+    <SidebarProvider>
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="flex gap-6">
+          <AppSidebar />
+          <div className="flex flex-col">
+            <SidebarTrigger className="hover:cursor-pointer py-4">
+              <ChevronLeft className="text-gray-400" />
+            </SidebarTrigger>
+            <div className="flex flex-col items-center gap-4 md:flex-row md:flex-wrap md:gap-8">
+              <ProductList products={products} />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 }
