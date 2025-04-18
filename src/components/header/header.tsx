fa8fa3be91d30b7d1e8d0cbc1cfd3c9d60ca8c0c
michelzaum <michel.oliveira999@hotmail.com>
2025-04-18 20:06:33 -0300

    feat: cart and header initial style

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,21 @@
+import Link from "next/link";
+import { ShoppingCart } from "lucide-react";
+
 export function Header() {
   return (
-    <header className="flex justify-center py-6">
-      <div className="w-full max-w-3xl flex justify-center items-center">
-        <h1>Header</h1>
+    <header className="flex justify-center py-10">
+      <div className="w-full max-w-lvw flex justify-center items-center px-10">
+        <div className="w-full flex justify-center gap-10">
+          <Link href="/">
+            <span>Início</span>
+          </Link>
+          <Link href="/profile">
+            <span>Perfil</span>
+          </Link>
+        </div>
+        <button className="justify-self-end hover:cursor-pointer">
+          <ShoppingCart />
+        </button>
       </div>
     </header>
   );
