fc051e4b3dd652e5bdbed270565ae820e2cba01b
michelzaum <michel.oliveira999@hotmail.com>
2025-04-25 23:30:48 -0300

    feat: quantity dropdown position adjustment

--- src/app/(pages)/cart-items/page.tsx
+++ src/app/(pages)/cart-items/page.tsx
@@ -86,41 +86,41 @@ export default function CartItems() {
                 <span>{formatCurrency(item.price)}</span>
               </div>
             </div>
-            <div>
-            <Select
-              defaultValue={`${item.quantity}`}
-              onValueChange={(value) => handleUpdateProductQuantity(item.id, Number(value))}
-            >
-              <SelectTrigger>
-                <SelectValue placeholder='Selecione a quantidade' />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Quantidade</SelectLabel>
-                  <SelectItem value="1">1</SelectItem>
-                  <SelectItem value="2">2</SelectItem>
-                  <SelectItem value="3">3</SelectItem>
-                  <SelectItem value="4">4</SelectItem>
-                  <SelectItem value="5">5</SelectItem>
-                  <SelectItem value="6">6</SelectItem>
-                  <SelectItem value="7">7</SelectItem>
-                  <SelectItem value="8">8</SelectItem>
-                  <SelectItem value="9">9</SelectItem>
-                  <SelectItem value="10">10</SelectItem>
-                  <SelectItem value="11">11</SelectItem>
-                  <SelectItem value="12">12</SelectItem>
-                  <SelectItem value="13">13</SelectItem>
-                  <SelectItem value="14">14</SelectItem>
-                  <SelectItem value="15">15</SelectItem>
-                  <SelectItem value="16">16</SelectItem>
-                  <SelectItem value="17">17</SelectItem>
-                  <SelectItem value="18">18</SelectItem>
-                  <SelectItem value="19">19</SelectItem>
-                  <SelectItem value="20">20</SelectItem>
-                  <SelectItem value="0">Selecione a quantidade</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
+            <div className="pr-1">
+              <Select
+                defaultValue={`${item.quantity}`}
+                onValueChange={(value) => handleUpdateProductQuantity(item.id, Number(value))}
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder='Selecione a quantidade' />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Quantidade</SelectLabel>
+                    <SelectItem value="1">1</SelectItem>
+                    <SelectItem value="2">2</SelectItem>
+                    <SelectItem value="3">3</SelectItem>
+                    <SelectItem value="4">4</SelectItem>
+                    <SelectItem value="5">5</SelectItem>
+                    <SelectItem value="6">6</SelectItem>
+                    <SelectItem value="7">7</SelectItem>
+                    <SelectItem value="8">8</SelectItem>
+                    <SelectItem value="9">9</SelectItem>
+                    <SelectItem value="10">10</SelectItem>
+                    <SelectItem value="11">11</SelectItem>
+                    <SelectItem value="12">12</SelectItem>
+                    <SelectItem value="13">13</SelectItem>
+                    <SelectItem value="14">14</SelectItem>
+                    <SelectItem value="15">15</SelectItem>
+                    <SelectItem value="16">16</SelectItem>
+                    <SelectItem value="17">17</SelectItem>
+                    <SelectItem value="18">18</SelectItem>
+                    <SelectItem value="19">19</SelectItem>
+                    <SelectItem value="20">20</SelectItem>
+                    <SelectItem value="0">Selecione a quantidade</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
             </div>
           </div>
         )) : (
