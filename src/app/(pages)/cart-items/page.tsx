906a7e819c5d7dea5ea6703bcdc772a76dc0b54f
michelzaum <michel.oliveira999@hotmail.com>
2025-05-24 17:14:27 -0300

    feat: only display the delete all button if there is at least one product in the cart

--- src/app/(pages)/cart-items/page.tsx
+++ src/app/(pages)/cart-items/page.tsx
@@ -93,13 +93,15 @@ export default function CartItems() {
   return (
     <div className="flex justify-center w-full">
       <div className="flex flex-col gap-8 p-6 w-full max-w-2xl">
-        <div className="w-full flex justify-end py-2">
-          <button className="cursor-pointer" onClick={() => setIsDeleteAllItemsFromCartModalOpen(true)}>
-            <span className="text-sm text-red-500 font-semibold md:text-base">
-              Esvaziar carrinho
-            </span>
-          </button>
-        </div>
+        {cartItems.length > 0 && (
+          <div className="w-full flex justify-end py-2">
+            <button className="cursor-pointer" onClick={() => setIsDeleteAllItemsFromCartModalOpen(true)}>
+              <span className="text-sm text-red-500 font-semibold md:text-base">
+                Esvaziar carrinho
+              </span>
+            </button>
+          </div>
+        )}
         {cartItems.length > 0 ? cartItems.map((item) => (
           <div key={item.id} className="flex items-center justify-between p-2 rounded-lg border border-gray-300 relative">
             <button
