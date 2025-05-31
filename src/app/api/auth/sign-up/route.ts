857cdd62dc87e51995c4a459afa1540984e868ee
michelzaum <michel.oliveira999@hotmail.com>
2025-05-30 21:23:55 -0300

    feat: implement the feature to register a new user in database

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -1,5 +1,46 @@
-import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
-export async function POST() {
+const schema = z.object({
+  name: z.string().min(1),
+  email: z.string().email(),
+  password: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const { success, error, data } = schema.safeParse(body);
+
+  if (!success) {
+    return NextResponse.json(
+      { errors: error.issues },
+      { status: 400 },
+    );
+  }
+
+  const { email, name, password } = data;
+
+  const emailAlreadyInUse = await db.user.findUnique({
+    where: { email },
+    select: { id: true },
+  });
+
+  if (emailAlreadyInUse) {
+    return NextResponse.json(
+      { error: 'E-mail already in use' },
+      { status: 409 },
+    );
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password,
+    },
+  })
+
   return NextResponse.json({ ok: true }, { status: 201 });
 }
