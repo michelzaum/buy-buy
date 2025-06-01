f1f56a03c3842152eab425834a01d3fad3e8ea8e
michelzaum <michel.oliveira999@hotmail.com>
2025-06-01 00:06:11 -0300

    feat: navigate user to root path if they tries to access sign-in page being authenticated

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,17 @@
-export default function AuthLayout({
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const accessToken = (await cookies()).get('accessToken')?.value;
+
+  if (accessToken) {
+    return redirect('/');
+  }
+
   return (
     <div className='min-h-screen grid place-items-center p-4'>
       {children}
