55f7eb532b4be2484ffafe6e5b2799a1a0d49500
michelzaum <michel.oliveira999@hotmail.com>
2025-06-03 22:36:52 -0300

    feat: persist user info in session storage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,5 @@
-import { User } from "@/entities/User";
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 type CartItem = {
@@ -15,7 +14,7 @@ type BasicUserInfo = {
 
 type Store = {
   selectedProducts: CartItem[];
-  user: BasicUserInfo;
+  user: BasicUserInfo | undefined;
 };
 
 type Actions = {
@@ -26,28 +25,34 @@ type Actions = {
 
 export const useStore = create<Store & Actions>()(
   devtools(
-    immer((set) => ({
-      selectedProducts: [],
-      setSelectedProduct: ({ productId, quantity }: CartItem) => set(({ selectedProducts }) => {
-        const alreadySelectedProductIndex = selectedProducts.findIndex(
-          (item) => item.productId === productId
-        );
+    persist(
+      immer((set) => ({
+        selectedProducts: [],
+        setSelectedProduct: ({ productId, quantity }: CartItem) => set(({ selectedProducts }) => {
+          const alreadySelectedProductIndex = selectedProducts.findIndex(
+            (item) => item.productId === productId
+          );
 
-        if (alreadySelectedProductIndex >= 0) {
-          selectedProducts[alreadySelectedProductIndex].quantity += quantity;
-          return;
-        }
+          if (alreadySelectedProductIndex >= 0) {
+            selectedProducts[alreadySelectedProductIndex].quantity += quantity;
+            return;
+          }
 
-        selectedProducts.push({ productId, quantity });
-      }),
-      remoteProduct: (productId: string) => set(({ selectedProducts }) => ({
-        selectedProducts: selectedProducts.filter((product) => product.productId !== productId),
+          selectedProducts.push({ productId, quantity });
+        }),
+        remoteProduct: (productId: string) => set(({ selectedProducts }) => ({
+          selectedProducts: selectedProducts.filter((product) => product.productId !== productId),
+        })),
+        user: undefined,
+        setUser: (userData: BasicUserInfo) => set(() => ({
+          user: userData,
+        })),
       })),
-      user: {} as User,
-      setUser: (userData: BasicUserInfo) => set(() => ({
-        user: userData,
-      })),
-    })),
+      {
+        name: 'buy-buy-storage',
+        storage: createJSONStorage(() => sessionStorage),
+      },
+    ),
     {
       name: "buy-buy",
     }
