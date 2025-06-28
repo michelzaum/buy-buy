1ea95faa06254310d8ae7ea3455670c392f06397
michelzaum <michel.oliveira999@hotmail.com>
2025-06-27 22:00:39 -0300

    feat: check if the refresh token sent in request is valid in our database

--- src/app/api/auth/refresh-token/route.ts
+++ src/app/api/auth/refresh-token/route.ts
@@ -1,7 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+import { RefreshTokenRepository } from "./RefreshTokenRepository";
 
 export async function POST(request: NextRequest) {
+  // TODO: Check the possibility to transform this file in a class
+  // and receive this repository in contructor
+  const refreshTokenRepository = new RefreshTokenRepository();
+
   const schema = z.object({
     refreshToken: z.string().uuid(),
   });
@@ -13,6 +18,17 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(
       { error: 'Invalid refresh token' },
       { status: 400 });
+  }
+
+  const { refreshToken: refreshTokenId } = validateSchema.data;
+
+  const refreshToken = await refreshTokenRepository.findById(refreshTokenId);
+
+  if (!refreshToken) {
+    return NextResponse.json(
+      { error: "Invalid refresh token" },
+      { status: 401 },
+    );
   }
 
   return NextResponse.json({ request })
