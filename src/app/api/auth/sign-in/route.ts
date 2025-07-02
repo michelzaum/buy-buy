89acb502e94ec68a97493393991cd1f2af66fb80
michelzaum <michel.oliveira999@hotmail.com>
2025-07-01 21:21:55 -0300

    feat: create and return refresh token in sign-in api route

--- src/app/api/auth/sign-in/route.ts
+++ src/app/api/auth/sign-in/route.ts
@@ -5,6 +5,8 @@ import { sign } from 'jsonwebtoken';
 
 import { db } from "@/lib/db";
 import { env } from "@/app/config/env";
+import { EXP_TIME_IN_DAYS } from "@/lib/constants";
+import { RefreshTokenRepository } from "../refresh-token/RefreshTokenRepository";
 
 const schema = z.object({
   email: z.string().email(),
@@ -12,6 +14,7 @@ const schema = z.object({
 });
 
 export async function POST(request: NextRequest) {
+  const refreshTokenRepository = new RefreshTokenRepository();
   const body = await request.json();
 
   const { success, error, data } = schema.safeParse(body);
@@ -51,8 +54,13 @@ export async function POST(request: NextRequest) {
     { expiresIn: '2d' },
   );
 
+  const expiresAt = new Date();
+  expiresAt.setDate(expiresAt.getDate() + EXP_TIME_IN_DAYS);
+
+  const { id } = await refreshTokenRepository.create({ userId: user.id, expiresAt });
+
   const response = NextResponse.json(
-    { name: user.name, email: user.email },
+    { name: user.name, email: user.email, refreshToken: id },
     { status: 200 },
   );
 
