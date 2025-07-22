7d71529d60d075e95b725a46242162f8fda54fdb
michelzaum <michel.oliveira999@hotmail.com>
2025-07-21 23:01:27 -0300

    fix: issue when add a new item in Cart table, due to repeated userId

--- src/app/_actions/save-cart-items.ts
+++ src/app/_actions/save-cart-items.ts
@@ -1,7 +1,7 @@
 "use server";
 
-import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
 
 interface SaveCartItemsProps {
   productId: string;
@@ -26,17 +26,35 @@ export async function saveCartItems({ productId, quantity, userEmail }: SaveCart
     );
   }
 
-  const userCart = await db.cart.create({
-    data: {
+  // TODO: Analyze the possibility of moving the logic to create a cart
+  // In register user, as all the customer users will need to have a cart,
+  // So maybe would be better to already created one when the user creates an account.
+  let userCartId = "";
+  const userCart = await db.cart.findUnique({
+    where: {
       userId: user.id,
     },
   });
+
+  if (userCart) {
+    userCartId = userCart.id;
+  }
+
+  if (!userCart) {
+    const createdUserCart = await db.cart.create({
+      data: {
+        userId: user.id,
+      },
+    });
+
+    userCartId = createdUserCart.id;
+  }
 
   return await db.cartItem.create({
     data: {
       productId,
       quantity,
-      cartId: userCart.id,
+      cartId: userCartId,
     },
   });
 }
