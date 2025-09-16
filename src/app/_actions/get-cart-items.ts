4d7398a8780b6138575443526ffc5018e7083e9f
michelzaum <michel.oliveira999@hotmail.com>
2025-09-15 23:21:57 -0300

    feat: throw an error in getCartItems function if user is not authenticated

--- src/app/_actions/get-cart-items.ts
+++ src/app/_actions/get-cart-items.ts
@@ -2,17 +2,12 @@
 
 import { auth } from "@/lib/auth";
 import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
 
 export async function getCartItems() {
   const user = await auth();
 
   if (!user) {
-    return NextResponse.json({
-      error: 'Unauthorized',
-    }, {
-      status: 401,
-    });
+    throw new Error('Unauthorized');
   }
 
   return await db.cart.findUnique({
