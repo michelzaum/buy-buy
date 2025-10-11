b2bd6bed058119513620326ce52cec4fbb5d025d
michelzaum <michel.oliveira999@hotmail.com>
2025-10-11 17:48:35 -0300

    feat: create success and cancel pages

---
+++ src/app/cancel/page.tsx
@@ -0,0 +1,8 @@
+export default function CancelPage() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-2xl font-bold text-red-600">Payment Canceled</h1>
+      <p>Your order was not completed.</p>
+    </div>
+  );
+}
---
+++ src/app/success/page.tsx
@@ -0,0 +1,8 @@
+export default function SuccessPage() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-2xl font-bold text-green-600">Payment Successful!</h1>
+      <p>Thank you for your purchase 🎉</p>
+    </div>
+  );
+}
