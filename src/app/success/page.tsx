04fa166eae041c00f7589a81cc22e50893ac1fbd
michelzaum <michel.oliveira999@hotmail.com>
2025-10-13 22:21:16 -0300

    feat: add "go back home" button in success payment page

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,8 +1,15 @@
+import { Button } from "@/components/ui/button";
+
 export default function SuccessPage() {
   return (
-    <div className="text-center p-10">
-      <h1 className="text-2xl font-bold text-green-600">Payment Successful!</h1>
-      <p>Thank you for your purchase 🎉</p>
+    <div className="w-full h-full flex justify-center">
+      <div className="flex flex-col gap-4 max-w-2xl text-center p-10">
+        <h1 className="text-2xl font-bold text-green-600">Pagamento concluído com sucesso!</h1>
+        <p>Obrigado pela sua compra 🎉</p>
+        <Button className="h-14">
+          Voltar para a home
+        </Button>
+      </div>
     </div>
   );
 }
