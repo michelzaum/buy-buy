3eb9d7c936e8fe4b5c3e9aa646564e8eee1b4bdb
michelzaum <michel.oliveira999@hotmail.com>
2025-10-23 22:05:49 -0300

    feat: get min and max price from input using refs

--- src/components/sidebar/price-filter.tsx
+++ src/components/sidebar/price-filter.tsx
@@ -1,22 +1,34 @@
+'use client';
+
+import { useRef } from 'react';
+
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 
 export function PriceFilter() {
+  const minPriceRef = useRef<HTMLInputElement>(null);
+  const maxPriceRef = useRef<HTMLInputElement>(null);
+
+  function handleApplyFilter(): void {
+    console.log('Min Price:', minPriceRef.current?.value);
+    console.log('Max Price:', maxPriceRef.current?.value);
+  }
+
   return (
     <div className="flex flex-col gap-4 py-4">
       <div className="flex flex-col gap-2">
         <label className="text-sm text-gray-900" htmlFor="minPrice">
           Preço mínimo
         </label>
-        <Input id="minPrice" placeholder="Ex: 25,00" />
+        <Input ref={minPriceRef} id="minPrice" placeholder="Ex: 25,00" />
       </div>
       <div className="flex flex-col gap-2">
         <label className="text-sm text-gray-900" htmlFor="maxPrice">
           Preço máximo
         </label>
-        <Input id="maxPrice" placeholder="Ex: 1000,00" />
+        <Input ref={maxPriceRef} id="maxPrice" placeholder="Ex: 1000,00" />
       </div>
-      <Button className="hover:cursor-pointer">Aplicar</Button>
+      <Button className="hover:cursor-pointer" onClick={handleApplyFilter}>Aplicar</Button>
     </div>
   );
 }
