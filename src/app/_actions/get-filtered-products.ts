827a50684e627817e9752beec1f98bf57060a144
michelzaum <michel.oliveira999@hotmail.com>
2025-10-31 22:27:38 -0300

    feat: create getFilteredProducts server action

---
+++ src/app/_actions/get-filtered-products.ts
@@ -0,0 +1,25 @@
+'use server';
+
+import { db } from "@/lib/db";
+
+interface GetFilteredProductsParams {
+  category: string;
+  price: {
+    min: number;
+    max: number;
+  };
+}
+
+export async function getFilteredProducts(filter: GetFilteredProductsParams) {
+  return db.product.findMany({
+    where: {
+      price: {
+        gte: filter.price.min ? filter.price.min : 0,
+        lte: filter.price.max ? filter.price.max : 100000,
+      },
+      category: {
+        name: filter.category,
+      },
+    },
+  });
+}
