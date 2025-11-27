508fedde2ee82138ce5b96a97c45987bc59df13c
michelzaum <michel.oliveira999@hotmail.com>
2025-11-27 00:22:42 -0300

    refactor: organize dropdown-menu element in separated components and folder

---
+++ src/components/ui/dropdown-menu/dropdow-menu-portal.tsx
@@ -0,0 +1,9 @@
+import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
+
+export function DropdownMenuPortal({
+  ...props
+}: React.ComponentProps<typeof DropdownMenuPrimitive.Portal>) {
+  return (
+    <DropdownMenuPrimitive.Portal data-slot="dropdown-menu-portal" {...props} />
+  )
+}
