6f6d4aaab8f20d49c2bf323931ab61247771557a
michelzaum <michel.oliveira999@hotmail.com>
2025-11-26 23:58:06 -0300

    refactor: organize collapsible element in separated components and folder

---
+++ src/components/ui/collapsible/collapsible-content.tsx
@@ -0,0 +1,12 @@
+import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
+
+export function CollapsibleContent({
+  ...props
+}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>) {
+  return (
+    <CollapsiblePrimitive.CollapsibleContent
+      data-slot="collapsible-content"
+      {...props}
+    />
+  );
+}
