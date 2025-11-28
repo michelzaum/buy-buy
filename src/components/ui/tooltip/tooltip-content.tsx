3b76bfca47f18d75ad7f745e48bccde70061601b
michelzaum <michel.oliveira999@hotmail.com>
2025-11-28 10:29:11 -0300

    refactor: organize tooltip element in separated files and folder

--- src/components/ui/tooltip/tooltip-content.tsx
+++ src/components/ui/tooltip/tooltip-content.tsx
@@ -1,40 +1,7 @@
-"use client"
+import { cn } from "@/lib/utils";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 
-import * as React from "react"
-import * as TooltipPrimitive from "@radix-ui/react-tooltip"
-
-import { cn } from "@/lib/utils"
-
-function TooltipProvider({
-  delayDuration = 0,
-  ...props
-}: React.ComponentProps<typeof TooltipPrimitive.Provider>) {
-  return (
-    <TooltipPrimitive.Provider
-      data-slot="tooltip-provider"
-      delayDuration={delayDuration}
-      {...props}
-    />
-  )
-}
-
-function Tooltip({
-  ...props
-}: React.ComponentProps<typeof TooltipPrimitive.Root>) {
-  return (
-    <TooltipProvider>
-      <TooltipPrimitive.Root data-slot="tooltip" {...props} />
-    </TooltipProvider>
-  )
-}
-
-function TooltipTrigger({
-  ...props
-}: React.ComponentProps<typeof TooltipPrimitive.Trigger>) {
-  return <TooltipPrimitive.Trigger data-slot="tooltip-trigger" {...props} />
-}
-
-function TooltipContent({
+export function TooltipContent({
   className,
   sideOffset = 0,
   children,
@@ -57,5 +24,3 @@ function TooltipContent({
     </TooltipPrimitive.Portal>
   )
 }
-
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
---
+++ src/components/ui/tooltip/tooltip.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import * as React from "react";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+
+import { TooltipProvider } from "./tooltip-provider";
+
+export function Tooltip({
+  ...props
+}: React.ComponentProps<typeof TooltipPrimitive.Root>) {
+  return (
+    <TooltipProvider>
+      <TooltipPrimitive.Root data-slot="tooltip" {...props} />
+    </TooltipProvider>
+  )
+}
