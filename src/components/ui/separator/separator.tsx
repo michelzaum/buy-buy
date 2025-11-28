4c50cf81e5c1abde22af7a50d6bb8fe82ed87aec
michelzaum <michel.oliveira999@hotmail.com>
2025-11-28 10:05:06 -0300

    refactor: organize separator element in a separated folder

--- src/components/ui/separator/separator.tsx
+++ src/components/ui/separator/separator.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import * as React from "react"
-import * as SeparatorPrimitive from "@radix-ui/react-separator"
+import * as React from "react";
+import * as SeparatorPrimitive from "@radix-ui/react-separator";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Separator({
+export function Separator({
   className,
   orientation = "horizontal",
   decorative = true,
@@ -24,5 +24,3 @@ function Separator({
     />
   )
 }
-
-export { Separator }
