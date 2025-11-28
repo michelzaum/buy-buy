f6ff69224ff2d5091f650c87d7214960e46a6fc4
michelzaum <michel.oliveira999@hotmail.com>
2025-11-28 10:02:38 -0300

    refactor: organize select element in separated files and folder

---
+++ src/components/ui/select/select-label.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils";
+import * as SelectPrimitive from "@radix-ui/react-select";
+
+export function SelectLabel({
+  className,
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.Label>) {
+  return (
+    <SelectPrimitive.Label
+      data-slot="select-label"
+      className={cn("text-muted-foreground px-2 py-1.5 text-xs", className)}
+      {...props}
+    />
+  )
+}
---
+++ src/components/ui/select/select.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import * as React from "react";
+import * as SelectPrimitive from "@radix-ui/react-select";
+
+export function Select({
+  ...props
+}: React.ComponentProps<typeof SelectPrimitive.Root>) {
+  return <SelectPrimitive.Root data-slot="select" {...props} />
+}
