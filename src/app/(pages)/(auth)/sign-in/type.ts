33202f411f2e5b117335054de18f4238d077c1a0
michelzaum <michel.oliveira999@hotmail.com>
2025-12-01 23:06:43 -0300

    refactor: create SelectedProduct type

--- src/app/(pages)/(auth)/sign-in/type.ts
+++ src/app/(pages)/(auth)/sign-in/type.ts
@@ -3,3 +3,9 @@ import { z } from "zod";
 import { schema } from "./schema";
 
 export type FormData = z.infer<typeof schema>;
+
+export type SelectedProduct = {
+  productId: string;
+  quantity: number;
+  wasAddedByAuthenticatedUser?: boolean;
+};
