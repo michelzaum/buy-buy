f6eff0b07f16fdab640ae3bc0fbb405b5847cc17
michelzaum <michel.oliveira999@hotmail.com>
2025-12-04 23:15:15 -0300

    refactor: create useSuccessPayment to centralize success-payment logic

--- src/app/(pages)/success-payment/page.tsx
+++ src/app/(pages)/success-payment/page.tsx
@@ -1,26 +1,17 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-
 import { Button } from "@/components/ui/button";
-import { useStore } from "@/store/store";
-import { deleteAllCartItems } from "../../_actions/delete-all-cart-items";
+import { useSuccessPayment } from "./useSuccessPayment";
 
 export default function SuccessPage() {
-  const { removeAllProducts, user } = useStore();
-  const router = useRouter();
-
-  async function onGoBackHomeClick(): Promise<void> {
-    await deleteAllCartItems(user?.email || '');
-    removeAllProducts();
-
-    router.push('/');
-  }
+  const { onGoBackHomeClick } = useSuccessPayment();
 
   return (
     <div className="w-full h-full flex justify-center">
       <div className="flex flex-col gap-4 max-w-2xl text-center p-10">
-        <h1 className="text-2xl font-bold text-green-600">Pagamento concluído com sucesso!</h1>
+        <h1 className="text-2xl font-bold text-green-600">
+          Pagamento concluído com sucesso!
+        </h1>
         <p>Obrigado pela sua compra 🎉</p>
         <Button className="h-14 w-full hover:cursor-pointer" onClick={onGoBackHomeClick}>
           Voltar para a home
