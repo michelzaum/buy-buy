190e2e32a5d3cf9b73b860e2849948d21b3f59a7
michelzaum <michel.oliveira999@hotmail.com>
2025-12-04 21:39:57 -0300

    refactor: create NoProductFound component

---
+++ src/app/(pages)/(product)/_components/no-product-found.tsx
@@ -0,0 +1,7 @@
+export function NoProductFound() {
+  return (
+    <div className="flex flex-col gap-4 md:flex-row md:flex-wrap md:gap-8">
+      <span>Nenhum produto encontrado.</span>
+    </div>
+  )
+}
--- src/app/(pages)/(product)/_components/product-list.tsx
+++ src/app/(pages)/(product)/_components/product-list.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { useStore } from "@/store/store";
 import { ProductItem } from "./product-item";
 import { EmptyProductList } from "./empty-product-list";
+import { NoProductFound } from "./no-product-found";
 import { ProductListProps } from "../types";
 
 export function ProductList({ products, isSuggestedProduct }: ProductListProps) {
@@ -12,9 +13,7 @@ export function ProductList({ products, isSuggestedProduct }: ProductListProps) 
 
   if (products.length === 0) {
     return (
-      <div className="flex flex-col gap-4 md:flex-row md:flex-wrap md:gap-8">
-        <span>Nenhum produto disponível.</span>
-      </div>
+      <NoProductFound />
     );
   }
 
