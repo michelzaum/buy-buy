4fec5086de0ea7b203f1845ad08148e32b606e6d
michelzaum <michel.oliveira999@hotmail.com>
2025-12-10 22:36:11 -0300

    feat: display image and action buttons in profile page

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+
 export default function Profile() {
   return (
-    <div>
-      <span>Profile page</span>
+    <div className="flex justify-center h-dvh">
+      <div>
+        <div className="flex flex-col items-center gap-6">
+          <Image
+            width={200}
+            height={200}
+            src="https://github.com/shadcn.png"
+            alt="@shadcn"
+            className="rounded-full"
+          />
+          <div className="flex items-center gap-4">
+            <div>
+              <input type="file" className="hidden" />
+              <Button variant='secondary'>Editar foto</Button>
+            </div>
+            <Button variant='destructive'>Remover foto</Button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
