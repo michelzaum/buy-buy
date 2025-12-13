240a507239d6208d9e87447d39e5861a77fe6672
michelzaum <michel.oliveira999@hotmail.com>
2025-12-12 22:32:05 -0300

    feat: create route handler to get user information

---
+++ src/app/api/user/profile/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { verify } from "jsonwebtoken";
+
+import { db } from "@/lib/db";
+import { env } from "@/app/config/env";
+
+export async function GET(request: NextRequest) {
+  const accessToken = request.cookies.get('accessToken')?.value;
+
+  if (!accessToken) {
+    return NextResponse.json(
+      { error: 'Invalid access token' },
+      { status: 401 },
+    );
+  }
+
+  const { sub: userId } = verify(accessToken, env.jwtSecret);
+
+  if (!userId) {
+    return NextResponse.json(
+      { error: 'Invalid access token' },
+      { status: 401 },
+    );
+  }
+  
+  const user = await db.user.findUnique({
+    where: { id: userId as string },
+  });
+
+  return NextResponse.json({ body: user });
+}
