eef8d13dfe840c35e003962e4a7cd20e8e53a017
michelzaum <michel.oliveira999@hotmail.com>
2025-12-13 15:06:53 -0300

    feat: set 'password' and 'confirmPassword' fields as optional

--- src/app/(pages)/profile/schema.ts
+++ src/app/(pages)/profile/schema.ts
@@ -3,6 +3,6 @@ import { z } from "zod";
 export const schema = z.object({
   name: z.string().min(1, { message: 'Informe um nome' }),
   email: z.string().email({ message: 'E-mail inválido' }),
-  password: z.string().min(1, { message: 'Informe a senha' }).max(8),
-  confirmPassword: z.string().min(1, { message: 'Confirme a senha' }).max(8),
+  password: z.string().optional(),
+  confirmPassword: z.string().optional(),
 });
